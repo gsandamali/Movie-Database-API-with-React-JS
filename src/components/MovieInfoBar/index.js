7bd0491b13ee6702189c9f0eb8f184b8c406f1b0
Geethika Sandamali <86560608+gsandamali@users.noreply.github.com>
2022-01-04 10:12:17 +0530

    Add files via upload

---
+++ src/components/MovieInfoBar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes  from "prop-types";
+//Style
+import { Wrapper, Content } from "./MovieInfoBar.styles";
+//Helpers
+import { calcTime, convertMoney} from '../../helpers';
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+    <Wrapper>
+        <Content>
+            <div className="column">
+                <p>Running time: {calcTime(time)}</p>
+            </div>
+            <div className="column">
+                <p>Budget: {convertMoney(budget)}</p>
+            </div>
+            <div className="column">
+                <p>Revenue: {convertMoney(revenue)}</p>
+            </div>
+        </Content>
+    </Wrapper>
+
+);
+
+MovieInfoBar.propTypes = {
+    time: PropTypes.number,
+    budget: PropTypes.number,
+    revenue: PropTypes.number
+};
+
+export default MovieInfoBar;
